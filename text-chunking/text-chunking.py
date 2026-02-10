afb703246de3d802a70d6be42b741244fe57e4a9
Anurag Deo <92918449+Anurag-Deo@users.noreply.github.com>
2026-02-10 09:33:58 +0530

    Add solution for text-chunking

---
+++ text-chunking/text-chunking.py
@@ -0,0 +1,12 @@
+def text_chunking(tokens, chunk_size, overlap):
+    """
+    Split tokens into fixed-size chunks with optional overlap.
+    """
+    # Write code here
+    step = chunk_size-overlap
+    ans = []
+    for i in range(0,len(tokens),step):
+        ans.append(tokens[i:i+chunk_size])
+        if i+chunk_size >= len(tokens):
+            break
+    return ans
