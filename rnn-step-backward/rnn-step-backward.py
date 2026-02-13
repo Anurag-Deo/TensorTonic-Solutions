c01ae2d86bef25d846adfe7194c6f3d18c5aa2c5
Anurag Deo <92918449+Anurag-Deo@users.noreply.github.com>
2026-02-13 21:27:33 +0530

    Add solution for rnn-step-backward

---
+++ rnn-step-backward/rnn-step-backward.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def rnn_step_backward(dh, cache):
+    """
+    Returns:
+        dx_t: gradient wrt input x_t      (shape: D,)
+        dh_prev: gradient wrt previous h (shape: H,)
+        dW: gradient wrt W               (shape: H x D)
+        dU: gradient wrt U               (shape: H x H)
+        db: gradient wrt bias            (shape: H,)
+    """
+    # Write code here
+    x_t, h_prev, h_t, W, U, b = cache
+
+    x_t = np.asarray(x_t)
+    h_prev = np.asarray(h_prev)
+    h_t = np.asarray(h_t)
+    dh = np.asarray(dh)
+    W = np.asarray(W)
+    U = np.asarray(U)
+
+    dz = dh * (1-h_t**2)
+    dx_t = W.T @ dz
+    dh_prev = U.T @ dz
+    dW = np.outer(dz, x_t)
+    dU = np.outer(dz, h_prev)
+    db = dz
+    return dx_t, dh_prev, dW, dU, db
